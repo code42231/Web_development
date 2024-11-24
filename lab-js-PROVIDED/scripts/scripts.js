24724e8117b3b2cbedbfe7a71a3e2e9a9f17d39a
code42231 <David.Obi@edu.sait.ca>
2024-11-23 20:46:34 -0700

    final code

--- lab-js-PROVIDED/scripts/scripts.js
+++ lab-js-PROVIDED/scripts/scripts.js
@@ -1,4 +1,4 @@
-/****************** YOUR NAME: David Obi
+/****************** YOUR NAME: 
 
 The instructions describe the missing logic that is needed; you will translate these into JavaScript in the places indicated.
 
@@ -10,8 +10,8 @@ You are encouraged to use the provided naming convention for ease of review.
 /* create variables to hold the values for modelName and duration */
 
 // INSERT YOUR CODE HERE
-
-
+let modelName = 'XYZ';
+let duration = 1;
 
 
 
@@ -26,7 +26,17 @@ You are encouraged to use the provided naming convention for ease of review.
 */
 
 // INSERT YOUR CODE HERE
-
+function recalculate() {
+    let costLabel = document.getElementById("calculated-cost");
+    let totalCost = 0;
+    if (modelName === 'XYZ'){
+        totalCost = duration * 100;
+    } else if (modelName === "CPRG") {
+        totalCost = duration * 213;
+    }
+    
+    costLabel.innerHTML = "$" + totalCost;
+}
 
 
 
@@ -45,8 +55,24 @@ You are encouraged to use the provided naming convention for ease of review.
     // modelButton.addEventListener("click", changeModel);
 
 // INSERT YOUR CODE HERE
+let modelButton = document.getElementById("switch-model");
+
+function changeModel() {
+    let modelText = document.getElementById("model-text");
+
+    if (modelName === 'XYZ') {
+        modelName = "CPRG";
+        modelText.innerHTML = "Model CPRG";
+    } else if (modelName === "CPRG") {
+        modelName = 'XYZ';
+        modelText.innerHTML = "Model XYZ";
+    }
+
+    recalculate();
+}
 
 
+modelButton.addEventListener("click", changeModel);
 
 
 
@@ -64,6 +90,34 @@ You are encouraged to use the provided naming convention for ease of review.
 */
 
 // INSERT YOUR CODE HERE
+let durationButton = document.getElementById("change-duration");
 
+function changeDuration() {
+    let durationText = document.getElementById("duration-text");
+
+    let newDuration = prompt("Enter new duration in months:", duration);
+
+    newDuration = parseInt(newDuration);
+
+    if (isNaN(newDuration) || newDuration <= 0) {
+        newDuration = 1;
+
+    }
+
+    duration = newDuration;
+
+
+    if (duration === 1) {
+
+        durationText.innerHTML = duration + " month";
+
+    } else {
+        durationText.innerHTML = duration + " months";
+    }
+
+    recalculate();
+}
+
+durationButton.addEventListener("click", changeDuration);
 
 
